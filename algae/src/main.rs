b40ac89642e3eb01c1ee99bafd98c97114d9bc62
make2002 <ms.kern@gmx.ch>
2024-01-03 18:31:22 +0100

    signal linear independency

--- algae/src/main.rs
+++ algae/src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 
 mod array;
 mod ml;
+mod signal_processing;
 
 fn main() {
     env::set_var("RUST_BACKTRACE", "full");
---
+++ algae/src/signal_processing/mod.rs
@@ -0,0 +1,75 @@
+mod SignalProcessing {
+    use crate::array::array::Array;
+
+    pub fn is_linearly_dependent(mut signal_vec:Vec<Vec<f64>>) -> bool {
+        fn extend(v:&mut Vec<Vec<f64>>, signal_vec:&mut Vec<Vec<f64>>) -> bool {
+            let max_len = signal_vec.iter().map(|v| v.len()).max().unwrap_or(0);
+            let mut temp = Vec::<f64>::with_capacity(signal_vec.len());
+            let mut counter = 0;
+            for n in 0..signal_vec.len() {
+                if signal_vec[n].len() == max_len {
+                    match signal_vec[n].pop() {
+                        Some(s) => temp.push(s),
+                        None => {
+                            temp.push(0.0);
+                            counter += 1;
+                        }
+                    }
+                } else {
+                    temp.push(0.0);
+                    counter += 1;
+                }
+            }
+            v.push(temp);
+            counter < signal_vec.len()
+        }
+        if signal_vec.len() == 0 {
+            true
+        } else {
+            let mut casorati_mat = Vec::<Vec<f64>>::with_capacity(signal_vec.len());
+            while casorati_mat.len() < signal_vec.len() {
+                extend(&mut casorati_mat, &mut signal_vec);
+            }
+            let mut counter = 0;
+            while let Err(e) = Array::new_mat(casorati_mat.clone()).inv() {
+                casorati_mat.swap_remove(counter % signal_vec.len());
+                counter += 1;
+                if !extend(&mut casorati_mat, &mut signal_vec) {
+                    return false;
+                }
+            }
+            true
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::signal_processing::SignalProcessing::is_linearly_dependent;
+
+    #[test]
+    fn linear_independency_true() {
+        let actual = {
+            let signals = vec![
+                vec![1.0, 2.0, 1.0, 0.0, 0.0],
+                vec![0.0, 1.0, 2.0, 1.0, 0.0],
+                vec![0.0, 0.0, 1.0, 2.0, 1.0],
+            ];
+            is_linearly_dependent(signals)
+        };
+        assert!(actual);
+    }
+
+    #[test]
+    fn linear_independency_false() {
+        let actual = {
+            let signals = vec![
+                vec![1.0, 2.0, 1.0, 0.0, 0.0],
+                vec![0.0, 1.0, 2.0, 1.0, 0.0],
+                vec![0.0, 1.0, 2.0, 1.0, 0.0],
+            ];
+            is_linearly_dependent(signals)
+        };
+        assert!(!actual);
+    }
+}
